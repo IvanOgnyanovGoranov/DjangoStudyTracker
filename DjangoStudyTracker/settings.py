d5a78b5aa57e20a8381ab04a5f8572a82f1bf2a1
IvanOgnyanovGoranov <144436457+IvanOgnyanovGoranov@users.noreply.github.com>
2025-04-08 07:44:14 +0300

    Add myapp folder

--- DjangoStudyTracker/settings.py
+++ DjangoStudyTracker/settings.py
@@ -74,9 +74,13 @@ WSGI_APPLICATION = 'DjangoStudyTracker.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "study_tracker_db",
+        "USER": "postgres",
+        "PASSWORD": "Goranov2016",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
 
