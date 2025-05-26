0713bb7b6374a7e9b64446743dedb2fd4fc4da74
IvanOgnyanovGoranov <144436457+IvanOgnyanovGoranov@users.noreply.github.com>
2025-05-26 23:34:26 +0300

    add number redirect view

--- tracker/urls.py
+++ tracker/urls.py
@@ -4,7 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.main_menu, name='main_menu'),
-    path('manage_subjects', views.manage_subjects, name='manage_subjects'),
-    path('show_timer', views.show_timer, name='show_timer'),
-    path('view_stats', views.view_stats, name='view_stats'),
+    path('manage_subjects/', views.manage_subjects, name='manage_subjects'),
+    path('show_timer/', views.show_timer, name='show_timer'),
+    path('view_stats/', views.view_stats, name='view_stats'),
+    path('<int:menu_number>/', views.main_menu_redirecting_numbers, name='main_menu_redirecting_numbers'),
 ]
--- tracker/views.py
+++ tracker/views.py
@@ -1,11 +1,15 @@
 from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from tracker.models import Subject
 from django.urls import reverse
 
 
 # Create your views here.
-
+MENU_ITEMS = [
+    ('manage_subjects', 'Manage Subjects'),
+    ('show_timer',     'Start Timer'),
+    ('view_stats',     'View Stats'),
+]
 
 def main_menu(request):
     url1 = reverse('manage_subjects')
@@ -31,13 +35,15 @@ def show_timer(request):
 def view_stats(request):
     return HttpResponse("Here will be the stats!")
 
-def main_menu_redirecting_numbers(response, number):
-    valid_numbers = [1, 2, 3]
+#to fix in the traker\urls
+def main_menu_redirecting_numbers(request, menu_number):
+    max_choice = len(MENU_ITEMS)
+    if not (1 <= menu_number <= max_choice):
+        return HttpResponseNotFound('This number is out of range!')
 
-    if number not in valid_numbers:
-        return HttpResponse('This number is not valid')
-
-    # to be finished
+    view_name, _ = MENU_ITEMS[menu_number - 1]
+    url = reverse(view_name)
+    return HttpResponseRedirect(url)
 
 
 
