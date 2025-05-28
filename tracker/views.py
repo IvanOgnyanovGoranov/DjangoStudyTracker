70d084e0bec204cfa98f3edf268dac6d9b4bc86d
IvanOgnyanovGoranov <144436457+IvanOgnyanovGoranov@users.noreply.github.com>
2025-05-28 16:44:29 +0300

    add timer template

--- tracker/views.py
+++ tracker/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 from django.shortcuts import render, redirect
 from tracker.models import Subject
 from django.urls import reverse
-
+from django.template.loader import render_to_string
 
 # Create your views here.
 MENU_ITEMS = [
@@ -29,7 +29,8 @@ def manage_subjects(request):
 
 
 def show_timer(request):
-    return HttpResponse("Here will be the timer!")
+    response_data = render_to_string('timer.html')
+    return HttpResponse(response_data)
 
 
 def view_stats(request):
