05a637f14b35347d0bc5ddffca5c4cb92dd2e5ce
IvanOgnyanovGoranov <144436457+IvanOgnyanovGoranov@users.noreply.github.com>
2025-05-30 23:47:54 +0300

    Little tweaks

--- tracker/views/study.py
+++ tracker/views/study.py
@@ -7,7 +7,8 @@ from django.template.loader import render_to_string
 
 def start_studying(request):
     """User picks a study subject and is redirected to the timer."""
-    return HttpResponse("Here will be where user picks a subject and is redirected to the timer!")
+    subjects = Subject.objects.all()
+    return render(request, 'study/pick.html', {'subjects': subjects})
 
 
 def redirect_if_no_subject(request):
@@ -15,8 +16,17 @@ def redirect_if_no_subject(request):
     return HttpResponseRedirect(reverse('add_subject'))
 
 
-def show_timer(request):
-    return render(request, 'timer.html')
+def show_timer(request, pk):
+    """Shows the timer with the specific subject the user chose."""
+    try:
+        subject = Subject.objects.get(pk=pk)
+        return render(request, 'study/timer.html',
+                      {'subject': subject.name
+                       }
+                      )
+    except Subject.DoesNotExist:
+        return HttpResponseNotFound('Subject does not exist')
+
 
 def add_study_time(request):
     return HttpResponse("User adds the study time to the correspondent subject")
