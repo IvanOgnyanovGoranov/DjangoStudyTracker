88cd79f9ccb5c909b12d32a2c9f3befc99e7a8ad
IvanOgnyanovGoranov <144436457+IvanOgnyanovGoranov@users.noreply.github.com>
2025-05-31 23:18:53 +0300

    study path finished

--- tracker/views/study.py
+++ tracker/views/study.py
@@ -1,6 +1,8 @@
+from datetime import date
+
 from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
-from django.shortcuts import render, redirect
-from tracker.models import Subject
+from django.shortcuts import render, redirect, get_object_or_404
+from tracker.models import Subject, StudyProgress
 from django.urls import reverse
 from django.template.loader import render_to_string
 
@@ -18,16 +20,26 @@ def redirect_if_no_subject(request):
 
 def show_timer(request, pk):
     """Shows the timer with the specific subject the user chose."""
-    try:
-        subject = Subject.objects.get(pk=pk)
-        return render(request, 'study/timer.html',
-                      {'subject': subject.name
-                       }
-                      )
-    except Subject.DoesNotExist:
-        return HttpResponseNotFound('Subject does not exist')
+    subject = get_object_or_404(Subject, pk=pk)
+    return render(request, 'study/timer.html', {
+        'subject': subject,
+    })
 
 
-def add_study_time(request):
-    return HttpResponse("User adds the study time to the correspondent subject")
+def add_study_time(request, subject_id):
+    if request.method == "POST":
+        minutes = int(request.POST.get('minutes', 0))
+        subj = get_object_or_404(Subject, pk=subject_id)
+
+        # Find or create today's StudyProgress
+        progress, created = StudyProgress.objects.get_or_create(
+            subject=subj,
+            date=date.today(),
+            defaults={'time_studied': 0}
+        )
+        progress.time_studied += minutes
+        progress.save()
+
+        # Redirect back to the same timer page
+    return redirect('show_timer', pk=subject_id)
 
