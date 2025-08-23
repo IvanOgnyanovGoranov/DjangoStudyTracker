b06c4e8b762372c57652ab8273501713992fde1c
IvanOgnyanovGoranov <144436457+IvanOgnyanovGoranov@users.noreply.github.com>
2025-08-23 16:05:26 +0300

    form added

--- tracker/views/subjects.py
+++ tracker/views/subjects.py
@@ -6,27 +6,35 @@ from django.shortcuts import render, redirect, get_object_or_404
 from tracker.models import Subject, StudyProgress
 from django.urls import reverse
 from django.template.loader import render_to_string
+from django.contrib import messages
 
 
 def my_subjects(request):
     subjects = Subject.objects.all()
     return render(request, 'my_subjects.html', {'subjects': subjects})
 
-# This view will be changed to the main view stats URL
+
 def manage_subject(request, pk):
     """Shows subject info with the option to edit or delete it."""
     subject = get_object_or_404(Subject, pk=pk)
+
+    if request.method == 'POST':
+        entered_goal = request.POST['new_goal']
+        subject.daily_goal = entered_goal
+        subject.save()
+        messages.success(request, "Daily goal updated successfully!")
+
     subject_progress = StudyProgress.objects.filter(subject_id=subject.id).aggregate(total_minutes=Coalesce(Sum('time_studied'), Value(0)))
     return render(request, 'manage_subjects/subject_info.html', {'subject': subject, 'progress': subject_progress['total_minutes']})
 
 
 def add_subject(request):
     """User chooses what subject to add."""
-    # Additional checks to be made here
+    # Add pop up if user doesn't enter minutes
     if request.method == "POST":
         name = request.POST.get('subject_name', '').strip()
         goal = int(request.POST.get('daily_goal', 0))
-
+ 
         if not Subject.objects.filter(name__iexact=name).exists():
             Subject.objects.create(name=name, daily_goal=goal)
             return redirect('my_subjects')
