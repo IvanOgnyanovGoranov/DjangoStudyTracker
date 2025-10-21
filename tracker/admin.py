12fbef29cc14d12f333f9fa1b5886f7abe11d627
IvanOgnyanovGoranov <144436457+IvanOgnyanovGoranov@users.noreply.github.com>
2025-10-21 09:40:20 +0300

    admin panel improved

--- tracker/admin.py
+++ tracker/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from tracker.models import Subject, StudyProgress
+from tracker.models import Subject, StudyProgress, EditSubject
 
 
 # Register your models here.
@@ -9,15 +9,26 @@ class SubjectAdmin(admin.ModelAdmin):
     list_display = ('name',
                     'created_at',
                     'daily_goal',
+                    'user',
                     )
 
 
 @admin.register(StudyProgress)
 class StudyProgressAdmin(admin.ModelAdmin):
-    list_display = ('subject',
+    list_display = (
+        'subject',
+        'time_studied',
+        'studied_on',
                     )
 
 
+@admin.register(EditSubject)
+class EditSubjectAdmin(admin.ModelAdmin):
+    list_display = (
+        'subject',
+        'edited_at',
+        'new_daily_goal',
+    )
 
 
 
