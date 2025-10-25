46da77df9e3c2901e61a4d46871d14665d4973e2
IvanOgnyanovGoranov <144436457+IvanOgnyanovGoranov@users.noreply.github.com>
2025-10-25 15:57:16 +0300

    add unittest for EditSubject model

--- tracker/tests.py
+++ tracker/tests.py
@@ -2,12 +2,12 @@ import unittest
 from django.test import TestCase
 from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
-from .models import Subject, DAILY_GOAL_MSG
+from .models import Subject, DAILY_GOAL_MSG, EditSubject
+
 
 class SubjectModelTests(TestCase):
 
     def setUp(self):
-        # Create a test user
         self.user = User.objects.create_user(username='testuser', password='password')
 
     def test_daily_goal_negative_raises_validation_error(self):
@@ -25,12 +25,12 @@ class SubjectModelTests(TestCase):
     def test_daily_goal_valid_saves_correctly(self):
         subject = Subject(user=self.user, name="History", daily_goal=60)
         try:
-            subject.full_clean()  # should not raise
+            subject.full_clean()
             subject.save()
         except ValidationError:
             self.fail("full_clean() raised ValidationError unexpectedly!")
 
-        # Confirm object exists in DB
+
         self.assertEqual(Subject.objects.count(), 1)
         self.assertEqual(Subject.objects.first().daily_goal, 60)
 
@@ -38,8 +38,28 @@ class SubjectModelTests(TestCase):
 class EditSubjectTests(TestCase):
 
     def setUp(self):
-        pass
+        self.user = User.objects.create_user(username='testuser', password='password')
+        self.subject = Subject.objects.create(user=self.user, name='Math', daily_goal=30)
 
+    def test_edit_daily_goal_zero_raises_validation_error(self):
+        edit = EditSubject(subject=self.subject, new_daily_goal=0)
+        with self.assertRaises(ValidationError) as context:
+            edit.full_clean()
+        self.assertIn(DAILY_GOAL_MSG, str(context.exception))
+
+    def test_edit_daily_goal_max_raises_validation_error(self):
+        edit = EditSubject(subject=self.subject, new_daily_goal=1100)
+        with self.assertRaises(ValidationError) as context:
+            edit.full_clean()
+        self.assertIn(DAILY_GOAL_MSG, str(context.exception))
+
+    def test_edit_goal_valid_saves_correctly(self):
+        edit = EditSubject(subject=self.subject, new_daily_goal=180)
+        try:
+            edit.full_clean()
+            edit.save()
+        except ValidationError:
+            self.fail("full_clean() raised ValidationError unexpectedly!")
 
 
 if __name__ == '__main__':
