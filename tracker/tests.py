60556038afef364ec051099711dfdac744790db1
IvanOgnyanovGoranov <144436457+IvanOgnyanovGoranov@users.noreply.github.com>
2025-10-28 07:23:08 +0200

    add form unittests

--- tracker/tests.py
+++ tracker/tests.py
@@ -3,6 +3,7 @@ from django.test import TestCase
 from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
 from .models import Subject, DAILY_GOAL_MSG, EditSubject, StudyProgress, STUDY_TIME_MSG
+from .forms import AddSubjectForm, EditSubjectForm
 
 
 class SubjectModelTests(TestCase):
@@ -79,6 +80,52 @@ class SubjectsProgressModelTests(TestCase):
             progress.save()
         except ValidationError:
             self.fail("full_clean() raised ValidationError unexpectedly!")
+
+
+class EditSubjectFormTests(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='testuser', password='password')
+        self.subject = Subject.objects.create(user=self.user, name='Math', daily_goal=60)
+
+    def test_valid_daily_goal_successful(self):
+        form = EditSubjectForm(data={'new_daily_goal': 60}, instance=EditSubject(subject=self.subject))
+        self.assertTrue(form.is_valid())
+
+    def test_invalid_daily_goal_fails(self):
+        form = EditSubjectForm(data={'new_daily_goal': 0}, instance=EditSubject(subject=self.subject))
+        self.assertFalse(form.is_valid())
+        self.assertIn('new_daily_goal', form.errors)
+
+
+class AddSubjectFromTests(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='testuser', password='password')
+        self.subject = Subject.objects.create(user=self.user, name='Math', daily_goal=60)
+
+
+
+
+class AddSubjectFormTests(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='testuser', password='password')
+        self.subject = Subject.objects.create(user=self.user, name='Math', daily_goal=60)
+
+    def test_valid_subject_form_passes(self):
+        form = AddSubjectForm(data={'name': 'History', 'daily_goal': 45}, user=self.user)
+        self.assertTrue(form.is_valid())
+
+    def test_duplicate_subject_name_fails(self):
+        form = AddSubjectForm(data={'name': 'math', 'daily_goal': 30}, user=self.user)
+        self.assertFalse(form.is_valid())
+        self.assertIn("This subject already exists!", form.errors['name'][0])
+
+    def test_name_whitespace_is_stripped(self):
+        form = AddSubjectForm(data={'name': '  Physics  ', 'daily_goal': 40}, user=self.user)
+        self.assertTrue(form.is_valid())
+        cleaned_name = form.clean_name()
+        self.assertEqual(cleaned_name, 'Physics')
+
+
 
 if __name__ == '__main__':
     unittest.main()
