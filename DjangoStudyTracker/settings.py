0e722705e0ddc7e6a3115a9b9764ae0c5a0a49ce
IvanOgnyanovGoranov <144436457+IvanOgnyanovGoranov@users.noreply.github.com>
2025-11-04 06:04:18 +0200

    debug to false

--- DjangoStudyTracker/settings.py
+++ DjangoStudyTracker/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-bw4u*rtj5s126wp+nwk3t2cdt$p@!kpp_0wl4l%&b(t3lmxc0a'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
