5c01b35cf7098f6e87bab6d4ddb515422b83debe
IvanOgnyanovGoranov <144436457+IvanOgnyanovGoranov@users.noreply.github.com>
2025-11-08 07:07:48 +0200

    add allowed host

--- DjangoStudyTracker/settings.py
+++ DjangoStudyTracker/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'False').lower() == 'true'
 
-ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "127.0.0.1,localhost", "studytracker-env.eba-jnnpdh2n.eu-west-1.elasticbeanstalk.com").split(",")
 
 # Application definition
 
