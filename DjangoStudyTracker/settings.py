7cc3c6afea16e45545ca138cd8ce36b18cc44a35
IvanOgnyanovGoranov <144436457+IvanOgnyanovGoranov@users.noreply.github.com>
2025-11-09 14:28:36 +0200

    fix settings and add runtime.txt

--- DjangoStudyTracker/settings.py
+++ DjangoStudyTracker/settings.py
@@ -80,9 +80,8 @@ WSGI_APPLICATION = 'DjangoStudyTracker.wsgi.application'
 
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default=os.getenv('DATABASE_URL', 'sqlite:///db.sqlite3'),
-        conn_max_age=600
+    "default": dj_database_url.config(
+        default=os.environ.get("DATABASE_URL")
     )
 }
 
