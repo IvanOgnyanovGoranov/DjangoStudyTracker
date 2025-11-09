6d79a5b4896650e95cb608f9b0d576dc45303bb6
IvanOgnyanovGoranov <144436457+IvanOgnyanovGoranov@users.noreply.github.com>
2025-11-09 07:28:52 +0200

    final touches before deployment

--- DjangoStudyTracker/settings.py
+++ DjangoStudyTracker/settings.py
@@ -80,10 +80,9 @@ WSGI_APPLICATION = 'DjangoStudyTracker.wsgi.application'
 
 
 DATABASES = {
-    "default": dj_database_url.config(
-        default=os.getenv("DATABASE_URL", f"sqlite:///{BASE_DIR / 'db.sqlite3'}"),
-        conn_max_age=600,
-        conn_health_checks=True,
+    'default': dj_database_url.config(
+        default=os.getenv('DATABASE_URL'),
+        conn_max_age=600
     )
 }
 
